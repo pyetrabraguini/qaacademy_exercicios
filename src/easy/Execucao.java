03377c9e5cabbd87e11e6d6412ff732934dc3b14
pyetrabraguini <braguinipy@gmail.com>
2022-03-21 14:23:02 -0300

    Refatoração exercicio 03 Easy

--- src/easy/Execucao.java
+++ src/easy/Execucao.java
@@ -1,10 +1,17 @@
 package easy;
 
+import javax.swing.JOptionPane;
+
 public class Execucao {
     public static void main(String[] args) {
-        Exercicio02 exercicio02 = new Exercicio02();
-        String palavraDigitada = exercicio02.retornaPalavraDigitada("Pyetra Braguini");
-        System.out.println(palavraDigitada);
+        // Exercicio02 exercicio02 = new Exercicio02();
+        // String palavraDigitada = exercicio02.retornaPalavraDigitada("Pyetra Braguini");
+        // System.out.println(palavraDigitada);
+
+        String valorUm = JOptionPane.showInputDialog("Digite o primeiro valor");
+        String valorDois = JOptionPane.showInputDialog("Digite o segundo valor");
+         Exercicio03 exercicio03 = new Exercicio03(); //instanciando a classe do exercicio
+        System.out.println(exercicio03.retornaTrocado(valorUm, valorDois));
     }
     
 }
--- src/easy/Exercicio03.java
+++ src/easy/Exercicio03.java
@@ -1,15 +1,14 @@
 package easy;
 
-import javax.swing.JOptionPane;
 
 public class Exercicio03 {
-    public static void main(String[] args) {
-        String numero1;
-        String numero2;
-        numero1 = JOptionPane.showInputDialog("Digite um numero inteiro!");
-        numero2 = JOptionPane.showInputDialog("Digite um numero inteiro!");
-        System.out.println("O valor do primeiro numero é: "+numero2);
-        System.out.println("O valor do segundo numero é: "+numero1);
+    
+    String valorUm;
+    String valorDois;
+
+    public String retornaTrocado(String valorUm, String valorDois) {
+        return "O primeiro número é: " +valorDois + "\n" +
+        "O segundo número é: "+valorUm;
     }
     
 }
