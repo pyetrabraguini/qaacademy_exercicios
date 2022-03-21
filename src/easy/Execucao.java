db47293e602bb1d02b91550c748fce1960f80cec
pyetrabraguini <braguinipy@gmail.com>
2022-03-21 15:03:47 -0300

    Refatoração exercicio 04 Easy

--- src/easy/Execucao.java
+++ src/easy/Execucao.java
@@ -8,10 +8,16 @@ public class Execucao {
         // String palavraDigitada = exercicio02.retornaPalavraDigitada("Pyetra Braguini");
         // System.out.println(palavraDigitada);
 
-        String valorUm = JOptionPane.showInputDialog("Digite o primeiro valor");
-        String valorDois = JOptionPane.showInputDialog("Digite o segundo valor");
-         Exercicio03 exercicio03 = new Exercicio03(); //instanciando a classe do exercicio
-        System.out.println(exercicio03.retornaTrocado(valorUm, valorDois));
+        // String valorUm = JOptionPane.showInputDialog("Digite o primeiro valor");
+        // String valorDois = JOptionPane.showInputDialog("Digite o segundo valor");
+        //  Exercicio03 exercicio03 = new Exercicio03(); //instanciando a classe do exercicio
+        // System.out.println(exercicio03.retornaTrocado(valorUm, valorDois));
+
+        
+        String numeroDigitadoUsuario = JOptionPane.showInputDialog("Digite um número");
+        int numeroDigitado = Integer.parseInt(numeroDigitadoUsuario);
+        Exercicio04 exercicio04 = new Exercicio04();
+        System.out.println("O dobro de " +numeroDigitado +" = " +exercicio04.calcularDobro(numeroDigitado));
     }
     
 }
