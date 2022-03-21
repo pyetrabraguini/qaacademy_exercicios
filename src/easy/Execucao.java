4428c61f06a3a400df2157b28dd153a6dc5027a7
pyetrabraguini <braguinipy@gmail.com>
2022-03-21 10:19:23 -0300

    Transformação Exercicio 02 Easy - metodo com Retorno e com passagem de parametros

---
+++ src/easy/Execucao.java
@@ -0,0 +1,10 @@
+package easy;
+
+public class Execucao {
+    public static void main(String[] args) {
+        Exercicio02 exercicio02 = new Exercicio02();
+        String palavraDigitada = exercicio02.retornaPalavraDigitada("Pyetra Braguini");
+        System.out.println(palavraDigitada);
+    }
+    
+}
--- src/easy/Exercicio02.java
+++ src/easy/Exercicio02.java
@@ -1,11 +1,15 @@
 package easy;
 
-import javax.swing.JOptionPane;
 
 public class Exercicio02 {
-    public static void main(String[] args) {
-        String palavraDigitada;
-        palavraDigitada = JOptionPane.showInputDialog("Digite uma palavra!");
-        System.out.println("Voce digitou: "+palavraDigitada);
+    public String retornaPalavraDigitada (String palavraDigitada) {
+        return "Você digitou: "+palavraDigitada;
+    }  
     }
-}
+
+
+    // ANTES:
+    // public static void main(String[] args) {
+    //     String palavraDigitada;
+    //     palavraDigitada = JOptionPane.showInputDialog("Digite uma palavra!");
+    //     System.out.println("Voce digitou: "+palavraDigitada);
