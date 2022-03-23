d889ee7df1b38eec761ae347cf2b79d061d88974
pyetrabraguini <braguinipy@gmail.com>
2022-03-23 16:56:56 -0300

    Refatoração exercicio 01 - medium

---
+++ src/medium/Execucao.java
@@ -0,0 +1,10 @@
+package medium;
+
+public class Execucao {
+    public static void main(String[] args) {
+        Exercicio01 exercicio01 = new Exercicio01();
+        exercicio01.verificarParOuImpar();
+
+    }
+    
+}
